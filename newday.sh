236da8668fb5a1202219e0b37135a20653382958
Amarjeet Kumar <masteramarjeetkumar@gmail.com>
2021-12-03 00:28:43 +0530

    Basic setup

---
+++ newday.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# A script to create folder structure for every new day
+
+if [[ $# == 0 ]]
+then
+  echo "Enter Day no. as argument"
+else
+  dir="day$@"
+  input="./$dir/input.txt"
+  part1="./$dir/part1.py"
+  part2="./$dir/part2.py"
+
+  if [[ -d $dir ]]
+  then
+    echo "the folder for day $@ already exists"
+    exit 1
+  else
+    mkdir $dir
+    touch $input $part1 $part2
+  fi
+
+  vi $part1
+fi
